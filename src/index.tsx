c96a45592cacb0f51516a3193b10915794eba24b
seunghoon <zlzlzlmo@daum.net>
2024-05-04 14:54:33 +0900

    fetch plugin으로 분리 (리팩터)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
@@ -29,7 +30,7 @@ const App = () => {
         entryPoints: ["index.js"],
         bundle: true,
         write: false,
-        plugins: [unpkgPathPlugin(input)],
+        plugins: [fetchPlugin(input), unpkgPathPlugin()],
         define: {
           "process.env.NODE_ENV": '"production"',
           global: "window",
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,19 +1,6 @@
 import * as esbuild from "esbuild-wasm";
-import axios from "axios";
-import localForage from "localforage";
 
-const fileCache = localForage.createInstance({
-  name: "filecache",
-});
-
-(async () => {
-  await fileCache.setItem("color", "red");
-
-  const color = await fileCache.getItem("color");
-  console.log(color);
-})();
-
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
@@ -40,36 +27,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-      });
-
-      build.onLoad({ filter: /.*/, namespace: "a" }, async (args: any) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
-
-        const { data, request } = await axios.get(args.path);
-
-        const cachedResult = await localForage.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        if (cachedResult) {
-          return cachedResult;
-        }
-
-        const result: esbuild.OnLoadResult = {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname,
-        };
-
-        await localForage.setItem(args.path, result);
-        return result;
       });
     },
   };
