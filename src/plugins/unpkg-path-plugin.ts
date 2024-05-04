8c3af7afc1203131306b6d7d42860a7a6304f5ef
seunghoon <zlzlzlmo@daum.net>
2024-05-04 14:39:48 +0900

    정규표현식 사용하여 모듈 처리

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -17,32 +17,29 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
+      // index.js의 진입 파일 처리
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return {
+          path: "index.js",
+          namespace: "a",
+        };
+      });
+
+      // 모듈 내 상대 경로로 되어있는 패키지들 처리
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+
+      // 모듈의 메인 파일 처리
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
-
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-        // else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //     namespace: "a",
-        //   };
-        // }
       });
 
       build.onLoad({ filter: /.*/, namespace: "a" }, async (args: any) => {
