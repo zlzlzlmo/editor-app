640d0eaed96bd07cc505c31991a0fc16d9eddbb4
seunghoon <zlzlzlmo@daum.net>
2024-05-08 20:26:23 +0900

    eval 내 에러시 메시지 출력

--- src/index.tsx
+++ src/index.tsx
@@ -52,7 +52,13 @@ const App = () => {
       <div id="root"></div>
       <script>
         window.addEventListener('message',(event)=>{
-          eval(event.data);
+          try {
+            eval(event.data);
+          } catch(err) {
+            console.log(err);
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div>' + err + '</div>'
+          }
         },false)
       </script>
     </body>
