57866de6849758f35e9ab1eec12c61884ef50d66
seunghoon <zlzlzlmo@daum.net>
2024-05-08 23:35:08 +0900

    코드 에디터에 기본값 옵션 설정 후 parent에서 props drilling 하기

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,13 @@
 import MoncaoEditor from "@monaco-editor/react";
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  initialValue: string;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
   return (
     <MoncaoEditor
+      value={initialValue}
       theme="dark"
       language="javascript"
       height={"500px"}
