9514078766c2a182fe822911dd24b21f3bfdab44
seunghoon <zlzlzlmo@daum.net>
2024-05-09 14:34:16 +0900

    resizable 컨트롤 최소 최대 크기값 설정

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -8,7 +8,12 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     return (
-        <ResizableBox width={Infinity} height={300} resizeHandles={['s']}>
+        <ResizableBox 
+            minConstraints={[Infinity, 24]}
+            maxConstraints={[Infinity, window.innerHeight * 0.9]} 
+            width={Infinity} height={300} 
+            resizeHandles={['s']}
+        >
             {children}
         </ResizableBox>
     )
