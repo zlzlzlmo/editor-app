2e99600e34307a37d639a58388634701165c13b0
seunghoon <zlzlzlmo@daum.net>
2024-05-10 18:40:17 +0900

    text editor 렌더링 및 기본 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from "./components/code-cell";
+import TextEditor from './components/text-editor';
 
 const el = document.getElementById('root');
 
@@ -10,7 +11,8 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <CodeCell/>      
+      <TextEditor/>
+      {/* <CodeCell/>       */}
     </div>
   );
 };
