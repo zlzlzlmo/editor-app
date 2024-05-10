0b2447f30da95267674a3f9b572aa48b8c446192
seunghoon <zlzlzlmo@daum.net>
2024-05-10 14:38:40 +0900

    루트 컴포넌트 렌더링 구버전 안쓰기

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from "./components/code-cell";
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el!);
+
 const App = () => {
   return (
     <div>
@@ -9,5 +14,5 @@ const App = () => {
     </div>
   );
 };
+root.render(<App />);
 
-ReactDOM.render(<App />, document.querySelector("#root"));
