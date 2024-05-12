77b98ed9f491ead175755e21543546e749ea063e
seunghoon <zlzlzlmo@daum.net>
2024-05-12 17:39:26 +0900

    셀( 코드 편집, 텍스트 편집)들을 나열하는 cell-list 생성 및 렌더

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import ReactDOM from 'react-dom/client';
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./state";
+import CellList from "./components/cell-list";
 
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeCell from "./components/code-cell";
-import TextEditor from './components/text-editor';
-
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   return (
-    <div>
-      <TextEditor/>
-      {/* <CodeCell/>       */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <CellList />
+      </div>
+    </Provider>
   );
 };
-root.render(<App />);
 
+root.render(<App />);
