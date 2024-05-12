aa11ac7204d09bb1bffd1b0adcb8f8369494806e
seunghoon <zlzlzlmo@daum.net>
2024-05-12 17:47:53 +0900

    셀을 추가하는 컴포넌트 및 코드 작성

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import "./cell-list.css";
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
+import AddCell from "./add-cell";
 
 const CellList: React.FC = () => {
-  const cells = useTypedSelector(({ cells: { data, order } }) =>
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <CellListItem cell={cell} />
+      <AddCell previousCellId={cell.id} />
+    </Fragment>
   ));
-  return <div>{renderedCells}</div>;
+
+  return (
+    <div className="cell-list">
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
+      {renderedCells}
+    </div>
+  );
 };
 
 export default CellList;
